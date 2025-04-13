3d8556f87738342d672f6707011ade480fe3a9f2
AshwinSaklecha <ashwin.saklecha@spenza.com>
2025-04-13 18:36:58 +0530

    modified frontend

--- frontend/src/services/product.service.ts
+++ frontend/src/services/product.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Product } from '../types';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Log the API URL being used
 console.log('Using API URL:', API_URL);
